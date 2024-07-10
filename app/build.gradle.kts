ead83e6b400feb7d2fbbbe87c1384cc3e5f7dd63
Prashant <prashant.rane@kindredgroup.com>
2024-07-10 19:32:05 +0200

    Missing updates.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,5 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+
 }
--- app/src/main/java/com/example/escooters/MainActivity.kt
+++ app/src/main/java/com/example/escooters/MainActivity.kt
@@ -6,20 +6,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.escooters.presentation.ScootersListScreen
 import com.example.escooters.presentation.ScootersViewModel
 import com.example.escooters.ui.theme.EScootersTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val scootersViewModel: ScootersViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,7 +23,7 @@ class MainActivity : ComponentActivity() {
             EScootersTheme {
                 val uiState = scootersViewModel.scootersListState.collectAsState()
                 ScootersListScreen(uiState.value) {
-                    Toast.makeText(baseContext, "${it.name}", Toast.LENGTH_LONG).show()
+                    Toast.makeText(baseContext, it.name, Toast.LENGTH_LONG).show()
                 }
             }
         }
